87a1aa9249130f744ef89b0b438970ca63bfaa94
= <=>
2020-10-15 16:02:24 +0100

    working

--- app.js
+++ app.js
@@ -2,33 +2,25 @@ const path = require("path");
 const express = require("express"); 
 const app = express();
 const port = 5000; 
-
 app.use(express.static("public"));  
 app.use(express.json()); 
-
 const {
     getAllPlayers,
     addPlayer
 } = require('./models/players');
-
 app.listen(port, () => {
     console.log(`Example app listening at http://localhost:${port}`)
   }
 );
-
-app.get("/api/scoreboard", async function (req,res){
+app.get("/scoreboard", async function (req,res){
     const players = await getAllPlayers();
-    res.json({success: true, playload: players});
+    res.json({success: true, payload: players});
     console.log(players);
 });
-
-
-app.post("/api/scoreboard", async function (req,res){
+app.post("/scoreboard", async function (req,res){
     const { name } = req.body;
     const { score } = req.body;
     const id = await addPlayer(name, score);
     res.json({success: true, message: `player has been created with ${id}`})
     console.log("WORKING")
 });
-
-
--- models/players.js
+++ models/players.js
@@ -1,17 +1,12 @@
 const { query } = require("../db");
-
-
 async function getAllPlayers(){
     const result = await query (`SELECT * FROM scoreboard;`);
     return result.rows;
 }
-
 async function addPlayer(name, score){
     const result = await query (`INSERT INTO scoreboard ( name, score ) VALUES ($1, $2) RETURNING id;`, [name, score]) //check this is coming in the correct format
     return result.rows[0].id;  
 }
-
-
   module.exports = {
     getAllPlayers,
     addPlayer
